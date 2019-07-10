17c0a5b0bc4ec67991aac534049e123a6bb523c3
AldoAbdn <alistairaldo@hotmail.co.uk>
2019-07-10 23:07:28 +0100

    trying to fix 420

--- eetoenglish/app.py
+++ eetoenglish/app.py
@@ -25,7 +25,8 @@ auth.set_access_token(os.environ["KEY"], os.environ["SECRET"])
 api = tweepy.API(auth)
 
 streamlistener = StreamListener(api, appendage=os.environ["APPENDAGE"], hashtags=os.environ["HASHTAGS"], tweet_size=int(os.environ["TWEET_SIZE"]))
-stream = tweepy.Stream(auth=api.auth, listener=streamlistener)
+if __name__ == '__main__':
+    stream = tweepy.Stream(auth=api.auth, listener=streamlistener)
 while True:
     try:
         stream.filter(follow=['19765204',],track=['#EEBOTTEST'],stall_warnings=True)
